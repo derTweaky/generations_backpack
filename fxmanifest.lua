6b152537ecbd5523576c3b67a4d6b01a85544433
derTweaky <dertweaky@users.noreply.github.com>
2026-05-29 09:37:33 +0200

    feat_lation_ui_shared_scripts

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,11 +2,12 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'derTweaky'
-description 'Metadata-based Backpack System with Slot 6 expansion and illenium-appearance support'
+description 'Metadata-based Backpack System with Slot 6 expansion, lation_ui and illenium-appearance support'
 version '1.0.0'
 
 shared_scripts {
-    '@ox_lib/init.lua'
+    '@ox_lib/init.lua',
+    '@lation_ui/init.lua'
 }
 
 client_scripts {
@@ -19,6 +20,7 @@ server_scripts {
 
 dependencies {
     'ox_lib',
+    'lation_ui',
     'ox_inventory',
     'illenium-appearance'
 }
